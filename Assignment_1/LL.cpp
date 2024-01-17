67f340f627317993ddb67474c701b367031f0330
Henok Tilahun <henokytilahun@gmail.com>
2024-01-17 14:52:14 -0800

    Ass 1 beginning

---
+++ Assignment_1/LL.cpp
@@ -0,0 +1,53 @@
+#include "LL.h"
+
+template <class type>
+LL<type>::LL(const LL<type>& copy)
+{ 
+	
+}
+
+template <class type>
+const LL<type>& LL<type>::operator=(const LL<type>& rhs)
+{
+  if (this != &rhs)
+  {
+    //Your code here
+  }
+
+  return *this;
+}
+
+template <class type>
+LL<type>::~LL()
+{
+  
+}
+template <class type>
+void LL<type>::tailInsert(const type& data)
+{
+ 
+}
+
+template <class type>
+void LL<type>::tailInsert(const iterator& s, const iterator& t)
+{
+  
+}
+
+template <class type>
+void LL<type>::tailInsert(const iterator& s)
+{
+  
+}
+
+template <class type>
+void LL<type>::remove(iterator& it)
+{
+  
+}
+
+template <class type>
+void LL<type>::headRemove()
+{
+  
+}
---
+++ Assignment_1/main.cpp
@@ -0,0 +1,11 @@
+#include "LL.h"
+#include "LL.cpp"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  
+  return 0;
+}
